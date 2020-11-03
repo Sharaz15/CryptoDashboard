a4d149d96adc88dd03414f394c8885dc1af05b65
sharaz15 <shahraz15@gmail.com>
2020-11-03 14:17:17 +0000

    Modified fetching of ticker data to updating at specified interval (note this is commented out for now as to prevent accidental exceeding of max api calls permitted)

--- CryptoDash/wwwroot/js/vue-ticker.js
+++ CryptoDash/wwwroot/js/vue-ticker.js
@@ -4,10 +4,16 @@
         currencyArr: []
     },
     created() {
-        fetch('https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD')
-            .then(response => response.json())
-            .then(data => {
-                this.currencyArr = data.Data
-            })
+        this.getLatestPrices();
+        //this.interval = setInterval(() => this.getLatestPrices(), 60000);
+    },
+    methods: {
+        getLatestPrices() {
+            fetch('https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD')
+                .then(response => response.json())
+                .then(data => {
+                    this.currencyArr = data.Data
+                })
+        }
     }
 })
