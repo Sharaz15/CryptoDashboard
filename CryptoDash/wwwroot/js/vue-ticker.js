5fada488d5e1008fcfda6819065ea01c528961ef
sharaz15 <shahraz15@gmail.com>
2020-11-03 12:37:29 +0000

    vue ticker implemented

--- CryptoDash/wwwroot/js/vue-ticker.js
+++ CryptoDash/wwwroot/js/vue-ticker.js
@@ -1,4 +1,13 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿var app = new Vue({
+    el: '#currencyTicker',
+    data: {
+        currencyArr: []
+    },
+    created() {
+        fetch('https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD')
+            .then(response => response.json())
+            .then(data => {
+                this.currencyArr = data.Data
+            })
+    }
+})
