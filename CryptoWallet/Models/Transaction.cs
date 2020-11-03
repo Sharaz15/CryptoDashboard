45b9965eae595145a39bb51cc8463c39302cf097
sharaz15 <shahraz15@gmail.com>
2020-11-03 16:25:51 +0000

    * CryptoDash.sln: * Startup.cs: * Program.cs: * appsettings.json: * Currency.cs: * WeatherForecast.cs: * CryptoWallet.csproj: * appsettings.Development.json: * launchSettings.json: * WeatherForecastController.cs:
    * Transaction.cs: Scaffolding of web api project with Currency and
      Transaction models added

---
+++ CryptoWallet/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CryptoWallet.Models
+{
+    public class Transaction
+    {
+        public Transaction()
+        {
+        }
+
+        public string CurrencyName { get; set; }
+        public double Amount { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
