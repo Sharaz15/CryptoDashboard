d51e94faaa52f203c29487a60be13e63d46de1c9
sharaz15 <shahraz15@gmail.com>
2020-11-04 15:34:35 +0000

    Service added to manade database CRUD actions

---
+++ CryptoWallet/Services/WalletService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CryptoWallet.Models;
+
+namespace CryptoWallet.Services
+{
+    public class WalletService
+    {
+        public WalletService()
+        {
+        }
+
+        public List<Currencys> GetCurrencys()
+        {
+            using (var context = new CryptoDashDatabaseContext())
+            {
+                return context.Currencys.ToList();
+            }
+        }
+
+        public void AddOrUpdateCurrencyHolding(Currencys currency, Transactions transaction)
+        {
+            using (var context = new CryptoDashDatabaseContext())
+            {
+                if (!context.Currencys.Contains(currency))
+                {
+                    context.Currencys.Add(currency);
+                }
+                else
+                {
+                    context.Currencys.Update(currency);
+                }
+                context.Transactions.Add(transaction);
+
+                context.SaveChanges();
+            }
+        }
+
+        public List<Transactions> GetTransactions()
+        {
+            using (var context = new CryptoDashDatabaseContext())
+            {
+                return context.Transactions.ToList();
+            }
+        }
+    }
+}
