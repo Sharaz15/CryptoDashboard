700fd268c834d2d6de742d9a5f7ad8da93c311ec
sharaz15 <shahraz15@gmail.com>
2020-11-04 18:47:10 +0000

    transactions controller seperated out

---
+++ CryptoWallet/Controllers/TransactionsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CryptoWallet.Models;
+using CryptoWallet.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CryptoWallet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly ILogger<TransactionsController> _logger;
+        private readonly IWalletService WalletService;
+
+        public TransactionsController(ILogger<TransactionsController> logger, IWalletService walletService)
+        {
+            _logger = logger;
+            WalletService = walletService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(WalletService.GetTransactions().ToArray());
+        }
+    }
+}
